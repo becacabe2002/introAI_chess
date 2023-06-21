ce6cc5e4043dd303a189958046402e67885c8d54
rellasie <86995253+rellasie@users.noreply.github.com>
2023-06-21 21:28:32 +0700

    add score_material function

--- simpleChess/ai_moves.py
+++ simpleChess/ai_moves.py
@@ -1,7 +1,25 @@
 import random
 
-def find_best_move():
+piece_score = {"K": 0, "Q": 9, "R": 5, "B": 3, "N": 3, "p": 1} # assign scrores for each piece
+
+CHECKMATE = 1000
+STALEMATE = 0
+
+def find_best_move(game_state, valid_moves):
+    """
+    Find the best move based on material alone
+    """
     return
+
+def score_material(board):
+    score = 0
+    for row in board:
+        for square in row:
+            if square[0] == 'w':
+                score += piece_score[square[1]]
+            elif square[0] == 'b':
+                score -= piece_score[square[1]]
+    return score
 
 def find_random_move(valid_moves):
     """
