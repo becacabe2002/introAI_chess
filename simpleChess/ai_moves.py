6433848cf92788b9265a9d2c3c04b34572b2f994
rellasie <86995253+rellasie@users.noreply.github.com>
2023-06-21 16:12:51 +0700

    add ai_moves.py

---
+++ simpleChess/ai_moves.py
@@ -0,0 +1,10 @@
+import random
+
+def find_best_move():
+    return
+
+def findRandomMove(valid_moves):
+    """
+    Picks and returns a random valid move.
+    """
+    return random.choice(valid_moves)
