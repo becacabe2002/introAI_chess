26d909b3e1a1fd9943374e5f4f02c15de37451a9
rellasie <86995253+rellasie@users.noreply.github.com>
2023-06-22 10:27:59 +0700

    finish greedy algorithm in find_best_move function

--- simpleChess/ai_moves.py
+++ simpleChess/ai_moves.py
@@ -9,7 +9,26 @@ def find_best_move(game_state, valid_moves):
     """
     Find the best move based on material alone
     """
-    return
+    turn_multiplier = 1 if game_state.white_to_move else -1
+    max_score = -CHECKMATE # set negative to maximize score later on
+    best_move = None
+    for player_move in valid_moves:
+        game_state.make_move(player_move)
+        opponents_moves = game_state.get_valid_moves()
+        for opponents_move in opponents_moves:
+            game_state.make_move(opponents_move)
+            if game_state.check_mate:
+                score = -CHECKMATE
+            elif game_state.stale_mate:
+                score = STALEMATE
+            else:
+                score = -turn_multiplier * score_material(game_state.board)
+            if score > max_score:
+                max_score = score
+                best_move = player_move
+            game_state.undo_move()
+        game_state.undo_move()
+    return best_move
 
 def score_material(board):
     score = 0
