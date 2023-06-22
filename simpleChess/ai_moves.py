96b547a7d585330a49c776870b35d832c5dfb026
rellasie <86995253+rellasie@users.noreply.github.com>
2023-06-22 11:18:07 +0700

    add minmax algorithm (without recursion)

--- simpleChess/ai_moves.py
+++ simpleChess/ai_moves.py
@@ -1,34 +1,42 @@
 import random
 
-piece_score = {"K": 0, "Q": 9, "R": 5, "B": 3, "N": 3, "p": 1} # assign scrores for each piece
+piece_score = {"K": 0, "Q": 9, "R": 5, "B": 3,
+               "N": 3, "p": 1}  # assign scrores for each piece
 
 CHECKMATE = 1000
 STALEMATE = 0
+
 
 def find_best_move(game_state, valid_moves):
     """
     Find the best move based on material alone
     """
     turn_multiplier = 1 if game_state.white_to_move else -1
-    max_score = -CHECKMATE # set negative to maximize score later on
-    best_move = None
+    opponent_minmax_score = CHECKMATE
+    best_player_move = None
+    random.shuffle(valid_moves)
+
     for player_move in valid_moves:
         game_state.make_move(player_move)
         opponents_moves = game_state.get_valid_moves()
+        opponent_max_score = -CHECKMATE
         for opponents_move in opponents_moves:
             game_state.make_move(opponents_move)
             if game_state.check_mate:
-                score = -CHECKMATE
+                score = -turn_multiplier * CHECKMATE
             elif game_state.stale_mate:
                 score = STALEMATE
             else:
                 score = -turn_multiplier * score_material(game_state.board)
-            if score > max_score:
-                max_score = score
-                best_move = player_move
+            if score > opponent_max_score:
+                opponent_max_score = score
             game_state.undo_move()
+        if opponent_max_score < opponent_minmax_score:
+            opponent_minmax_score = opponent_max_score
+            best_player_move = player_move
         game_state.undo_move()
-    return best_move
+    return best_player_move
+
 
 def score_material(board):
     score = 0
@@ -39,6 +47,7 @@ def score_material(board):
             elif square[0] == 'b':
                 score -= piece_score[square[1]]
     return score
+
 
 def find_random_move(valid_moves):
     """
