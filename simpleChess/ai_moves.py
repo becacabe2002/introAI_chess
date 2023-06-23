dba8810c1704fc1f89ad92d57a221d969542420d
namdeptrai1102 <109681639+namdeptrai1102@users.noreply.github.com>
2023-06-23 21:35:31 +0700

    finish_nega_max_alpha_beta

--- simpleChess/ai_moves.py
+++ simpleChess/ai_moves.py
@@ -8,52 +8,55 @@ STALEMATE = 0
 DEPTH = 3
 
 
-def find_best_move(game_state, valid_moves):
-    """
-    Find the best move based on material alone
-    """
-    turn_multiplier = 1 if game_state.white_to_move else -1
-    opponent_minmax_score = CHECKMATE
-    best_player_move = None
-    random.shuffle(valid_moves)
+# def find_best_move_min_max_no_recursion(game_state, valid_moves):
+#     """
+#     Find the best move, min max without recursion
+#     """
+#     turn_multiplier = 1 if game_state.white_to_move else -1
+#     opponent_minmax_score = CHECKMATE
+#     best_player_move = None
+#     random.shuffle(valid_moves)
 
-    for player_move in valid_moves:
-        game_state.make_move(player_move)
-        opponents_moves = game_state.get_valid_moves()
-        if game_state.stale_mate:
-            opponent_max_score = STALEMATE
-        elif game_state.check_mate:
-            opponent_max_score = -CHECKMATE
-        else:
-            opponent_max_score = -CHECKMATE
-            for opponents_move in opponents_moves:
-                game_state.make_move(opponents_move)
-                game_state.get_valid_moves()
-                if game_state.check_mate:
-                    score = CHECKMATE
-                elif game_state.stale_mate:
-                    score = STALEMATE
-                else:
-                    score = -turn_multiplier * score_material(game_state.board)
-                if score > opponent_max_score:
-                    opponent_max_score = score
-                game_state.undo_move()
-        if opponent_max_score < opponent_minmax_score:
-            opponent_minmax_score = opponent_max_score
-            best_player_move = player_move
-        game_state.undo_move()
-    return best_player_move
+#     for player_move in valid_moves:
+#         game_state.make_move(player_move)
+#         opponents_moves = game_state.get_valid_moves()
+#         if game_state.stale_mate:
+#             opponent_max_score = STALEMATE
+#         elif game_state.check_mate:
+#             opponent_max_score = -CHECKMATE
+#         else:
+#             opponent_max_score = -CHECKMATE
+#             for opponents_move in opponents_moves:
+#                 game_state.make_move(opponents_move)
+#                 game_state.get_valid_moves()
+#                 if game_state.check_mate:
+#                     score = CHECKMATE
+#                 elif game_state.stale_mate:
+#                     score = STALEMATE
+#                 else:
+#                     score = -turn_multiplier * score_material(game_state.board)
+#                 if score > opponent_max_score:
+#                     opponent_max_score = score
+#                 game_state.undo_move()
+#         if opponent_max_score < opponent_minmax_score:
+#             opponent_minmax_score = opponent_max_score
+#             best_player_move = player_move
+#         game_state.undo_move()
+#     return best_player_move
 
 
 def find_best_move(game_state, valid_moves):
     """
     Helper method to make first recursive call
     """
-    global next_move
+    global next_move, counter
     next_move = None
     random.shuffle(valid_moves)
+    counter = 0
     #find_move_minmax(game_state, valid_moves, DEPTH, game_state.white_to_move)
+    #find_move_nega_max(game_state, valid_moves, DEPTH, game_state.white_to_move)
     find_move_nega_max_alpha_beta(game_state, valid_moves, DEPTH, -CHECKMATE, CHECKMATE, 1 if game_state.white_to_move else -1)
+    print(counter)
     return next_move
 
 
@@ -89,8 +92,27 @@ def find_move_minmax(game_state, valid_moves, depth, white_to_move):
             game_state.undo_move()
         return min_score
 
+def find_move_nega_max(game_state, valid_moves, depth, turn_multiplier):
+    global next_move, counter
+    counter += 1
+    if depth ==0:
+        return turn_multiplier + score_board(game_state)
+    
+    max_score =-CHECKMATE
+    for move in valid_moves:
+        game_state.make_move(move)
+        next_moves = game_state.get_valid_moves()
+        score = -find_move_nega_max(game_state, next_moves, depth -1, -turn_multiplier)
+        if score > max_score:
+            max_score = score
+            if depth == DEPTH:
+                next_move = move
+        game_state.undo_move()
+    return max_score
+
 def find_move_nega_max_alpha_beta(game_state, valid_moves, depth, alpha, beta, turn_multiplier):
-    global next_move
+    global next_move, counter
+    counter += 1
     if depth ==0:
         return turn_multiplier + score_board(game_state)
     
